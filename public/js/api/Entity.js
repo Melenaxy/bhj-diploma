9db47126db28bb65cde125700fee42bc28e431b2
MelnikovaEYu <MelnikovaEYu@ufa.infteh.ru>
2024-03-05 00:12:57 +0500

    Реализованы функции createRequest.js, Entity.js

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -3,13 +3,17 @@
  * Имеет свойство URL, равно пустой строке.
  * */
 class Entity {
+  static URL = '';
   /**
    * Запрашивает с сервера список данных.
    * Это могут быть счета или доходы/расходы
    * (в зависимости от того, что наследуется от Entity)
    * */
-  static list(data, callback){
-
+  static list(data, callback) {
+    let options = {
+      url: this.URL, data, method: 'GET', callback
+    };
+    createRequest(options);
   }
 
   /**
@@ -18,14 +22,14 @@ class Entity {
    * что наследуется от Entity)
    * */
   static create(data, callback) {
-
+    createRequest(options = { url: this.URL, data, method: 'PUT', callback })
   }
 
   /**
    * Удаляет информацию о счёте или доходе/расходе
    * (в зависимости от того, что наследуется от Entity)
    * */
-  static remove(data, callback ) {
-
+  static remove(data, callback) {
+    createRequest(options = { url: this.URL, data, method: 'DELETE', callback })
   }
 }
--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,28 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
+const createRequest = (options = { url, data, method, callback }) => {
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    let url = `${options.url}?`;
+    let formData = new FormData();
+    if (options.method === 'GET') {
+        for (let key in options.data) {
+            url += `${key}=${options.data[key]}&`;
+            //`abc?prop=значение&login=вфыв&id=123`
+        }
+    } else {
+        for (let key in options.data) {
+            formData.append(key, options.data[key]);
+        }
+    };
 
+    try {
+        xhr.open(options.method, url);
+        xhr.send(formData);
+        xhr.addEventListener('load', (e) => options.callback(null, xhr.response));
+    }
+    catch (err) {
+        options.callback(err);
+    };
 };
